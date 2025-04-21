d23ccdd8d03f24d4420509647af2cfeb77424e95
carlos alberto <carlosalbertosnb20@gmail.com>
2025-04-20 23:29:32 -0300

    WW

--- index.js
+++ index.js
@@ -11,53 +11,73 @@ lista_produtos.lista_produtos.map((produto) => {
 const gal = document.querySelector('.container');
 const carro = document.querySelector('.carrosel');
 
-gal.style.display = 'flex';
-gal.style.overflow  = 'hidden';
 
-//carro.style.position = 'absolute';
+
 carro.style.display = 'flex';
-carro.style.gap = '10px';
-//carro.style.overflow = 'hidden';
-//carro.style.left = '100px';
+carro.style.position = 'relative';
+carro.style.width = '100%';
+carro.style.height = '100%';
+
+
+gal.style.display = 'flex';
+gal.style.flexDirection = 'column';
+gal.style.background = 'rgb(85, 149, 136)';
 
 
 
-arrayimg.map((imagem) => {
 
+
+arrayimg.map((imagem,index) => {
+    
     const div = document.createElement('div');
     div.setAttribute('class', 'item');
+    div.setAttribute('id', `itemtempo${index}`);
     const img = document.createElement('img');
     img.src = `${imagem}`;
     img.width = 150;
     img.height = 150;
-
     gal.appendChild(div);
     carro.appendChild(div);
-    div.appendChild(img);
-    
-   
+    div.appendChild(img); 
 });
 
-const itens = [...document.querySelectorAll('.item')];
-
-
-
-var vr = -0;
-var x = 0;
-
-
-const evento = () => {
-
-    itens.forEach((item) => {
-        console.log(item);
-        
-    
-    });
-
-    //setInterval(evento, 100);
+const style = document.createElement('style');
+style.textContent = `
+@keyframes move {
+    from { left: 70%; }
+    to { left: 0; }
 }
+`;
 
-evento();
+document.head.appendChild(style);
+const divtempo = [...document.querySelectorAll('#itemtempo')];
+const divimtem = [...document.querySelectorAll('.item')];
+
+
+
+
+divimtem.forEach((item) => {
+   
+    item.style.position = 'absolute';
+    item.style.left = '100%';
+    item.style.transition = '5s';
+    item.animationDelay = '1s';
+    
+    item.style.animation = 'move 5s linear infinite';
+});   
+
+divtempo.forEach((item) => {
+    console.log(item);
+});
+
+
+
+
+
+
+
+
+
 
 
 //itens[0].style.position = 'relative';
